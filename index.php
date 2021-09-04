73ad82a69da7726338cd43d2d3fafec319d8705c
Jason Grant <jason@flexewebs.com>
2021-09-04 14:05:06 +0100

    Added ability to define colour scheme through URL parameter (e.g. ?scheme=3)

--- index.php
+++ index.php
@@ -14,38 +14,49 @@
 <body>
     <div class="artCanvas">
     <?php 
-    
-    // Colours to use 
-    //$colors = array("#C90C0C", "#171504"); // Red, black
-    //$colors = array("#C90C0C", "#171504", "#FFCE1F"); // Red, black, yellow
-    //$colors = array("#C90C0C", "#171504", "#FFCE1F", "#FFF"); // Red, black, yellow, white
-    //$colors = array("#384085", "#04A7D0", "#A6D2C8", "#F8EDE0", "#FEF6E3", "#FFCBBF"); // Mixed pallette
-    //$colors = array("#405CD6", "#4D81DA", "#5BA8E1", "#65C3E5", "#6ACDEB"); // All blue
-    $colors = array("#0C0727", "#F7BB13", "#EE5698", "#7EC064", "#D2ACDC", "#FBA305", "#E14699", "#36A86C", "#DE5111", "#CF297B", "#E85A23", "#C0221A", "#002970", "#E89915", "#FE5FB0"); // Van Gogh Starry Night colour picks
+
+    // Colour schemes to paint with
+    $colourSchemes = array (0 => array("#C90C0C", "#171504"), // Red, black 
+                            1 => array("#C90C0C", "#171504", "#FFCE1F"), // Red, black, yellow
+                            2 => array("#C90C0C", "#171504", "#FFCE1F", "#FFF"), // Red, black, yellow, white
+                            3 => array("#384085", "#04A7D0", "#A6D2C8", "#F8EDE0", "#FEF6E3", "#FFCBBF"), // Mixed pallette                            
+                            4 => array("#405CD6", "#4D81DA", "#5BA8E1", "#65C3E5", "#6ACDEB"), // All blue
+                            5 => array("#0C0727", "#F7BB13", "#EE5698", "#7EC064", "#D2ACDC", "#FBA305", "#E14699", "#36A86C", "#DE5111", "#CF297B", "#E85A23", "#C0221A", "#002970", "#E89915", "#FE5FB0"),
+                        );
+
+    // Colour schemes to use 
+    if(isset($_GET['scheme'])) { 
+        if($_GET['scheme']<0) { 
+            $scheme = 0;    
+        }
+        $scheme = $_GET['scheme'];
+    }
+    else 
+    { 
+        $scheme = 0;
+    }    
 
     // Number of slots
     $slots = 190; 
-    $slotsInRow = 10;
-    $totalColours = count($colors)-1;
 
-    
+    // Total number of colours 
+    $totalColours = count($colourSchemes[$scheme])-1;
 
+    // Generate the art canvas slots 
     for($i=1; $i<$slots+1; $i++) {   
                 
-        if($i%2==0) {
-            $circle = "circle";
-        }
-        else { 
-            $circle = "";
-        }
+        // Decide to create slot as square or a circle
+        if($i%2==0) { $circle = "circle"; }
+        else { $circle = ""; }
 
-        $squareColour = $colors[rand(0,$totalColours)];
-        $blobColour = $colors[rand(0,$totalColours)];
+        // Assign slot and blob colour
+        $squareColour = $colourSchemes[$scheme][rand(0,$totalColours)];
+        $blobColour = $colourSchemes[$scheme][rand(0,$totalColours)];
 
-        // Make blob a different colour from square
+        // Make sure blob is of a different colour from square
         if($squareColour==$blobColour) {            
             while($squareColour==$blobColour) {
-                $blobColour = $colors[rand(0,$totalColours)];                
+                $blobColour = $colourSchemes[$scheme][rand(0,$totalColours)];                
             }
         }
         ?>
