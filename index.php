70f3b4b17b67c0e0ba3b9075c7b8d60619f8caa7
Jason Grant <jason@flexewebs.com>
2021-09-04 13:28:32 +0100

    Making it full screen size

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
 <head>
     <title>Funky squares</title>
     <style>
-        .artCanvas { float: left; width: 500px; height: 500px; }
-        .slot { float: left; width: 50px; height: 50px; margin: 0; padding: 0; }         
+        * { margin: 0; padding: 0; }
+        .artCanvas { float: left; width: 100vw; height: 100vh; }
+        .slot { float: left; width: 10vh; height: 10vh; margin: 0; padding: 0; }         
         .circle { border-radius: 50%; }
     </style>
 </head>
@@ -23,7 +24,7 @@
     $colors = array("#0C0727", "#F7BB13", "#EE5698", "#7EC064", "#D2ACDC", "#FBA305", "#E14699", "#36A86C", "#DE5111", "#CF297B", "#E85A23", "#C0221A", "#002970", "#E89915", "#FE5FB0"); // Van Gogh Starry Night colour picks
 
     // Number of slots
-    $slots = 100; 
+    $slots = 190; 
     $slotsInRow = 10;
     $totalColours = count($colors)-1;
 
